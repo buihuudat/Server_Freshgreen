118318b3799ce894bb93a884cb055591cd948d93
Bùi Hữu Đạt <42347089+buihuudat@users.noreply.github.com>
2023-08-17 20:36:16 +0700

    Update faqController.js
    fix bugs

--- controllers/faqController.js
+++ controllers/faqController.js
@@ -39,7 +39,7 @@ const faqController = {
       });
       return res.status(200).json(data);
     } catch (error) {
-      res.status(500).json(error);
+      return res.status(500).json(error);
     }
   },
 };
