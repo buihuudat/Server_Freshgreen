ec9c08bb9ac8d4c06b09a1cd992496632de46c16
BuiHuuDat <dat54261001@gmail.com>
2024-01-13 17:00:00 +0700

    push

--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -3,16 +3,17 @@ const tokensNotification = require("../models/tokensNotification");
 const tokenMiddleware = async (req, res, next) => {
   try {
     const tokens = await tokensNotification.find();
-    if (!tokens) {
+    if (!tokens.length) {
       await tokensNotification.create({
         tokens: [],
         devices: { mobile: [], web: [] },
       });
     } else {
       req.tokenId = tokens[0]._id;
-      next();
     }
+    next();
   } catch (error) {
+    console.log(error);
     return false;
   }
 };
